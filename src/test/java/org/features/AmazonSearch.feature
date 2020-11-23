59ebfc425a0281da1b2aa6c7499848e39d15a491
Katrina Ka <katrinakazakov@gmail.com>
2020-11-22 19:43:18 -0600

    updated ExtentReport

--- src/test/java/org/features/AmazonSearch.feature
+++ src/test/java/org/features/AmazonSearch.feature
@@ -8,4 +8,3 @@ And Searches for ‘Alexa’
 And navigates to the second page
 And selects the third item
 Then assert that the item would be available for purchase
-Then close the browser
--- src/test/java/org/stepDefinitions/AmazonSearchStepDefinitions.java
+++ src/test/java/org/stepDefinitions/AmazonSearchStepDefinitions.java
@@ -1,5 +1,7 @@
 package org.stepDefinitions;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import org.configuration.Setup;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -8,41 +10,48 @@ import org.pageObjects.ItemPage;
 import org.pageObjects.SearchResultsPage;
 
 public class AmazonSearchStepDefinitions extends Setup {
+    HeaderPage h;
+    SearchResultsPage sr;
+    ItemPage ip;
+
+    @Before
+    public void beforeScenario() {
+        driver = initializeDriver();
+    }
 
     @Given("^the user navigates to amazon$")
     public void the_user_navigates_to_amazon()  {
-        driver = initializeDriver();
         hitUrl("https://www.amazon.com");
     }
 
     @Given("^Searches for ‘Alexa’$")
     public void searches_for_Alexa()  {
-        HeaderPage h=new HeaderPage(driver);
+        h=new HeaderPage(driver);
         h.searchFor("alexa");
     }
 
     @Given("^navigates to the second page$")
     public void navigates_to_the_second_page()  {
-        SearchResultsPage sr= new SearchResultsPage(driver);
+        sr= new SearchResultsPage(driver);
         sr.selectPage("2");
     }
 
     @Given("^selects the third item$")
     public void selects_the_third_item()  {
-        SearchResultsPage sr= new SearchResultsPage(driver);
+        sr= new SearchResultsPage(driver);
         String product = sr.selectListItem(3);
-        ItemPage ip = new ItemPage(driver);
+        ip = new ItemPage(driver);
         ip.verifyProductTitle(product);
     }
 
     @Then("^assert that the item would be available for purchase$")
     public void assert_that_the_item_would_be_available_for_purchase() {
-        ItemPage ip = new ItemPage(driver);
+        ip = new ItemPage(driver);
         ip.verifyAddtoCartIsEnabled();
     }
 
-    @Then("^close the browser$")
-    public void close_the_browser() {
+    @After
+    public void afterScenario() {
         driver.quit();
     }
 }
